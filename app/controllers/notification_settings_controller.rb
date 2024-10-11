0f4781050deb125185085b47f0f538133e59e306
Sankhlavinay <vinaysankhla4233@gmail.com>
2024-10-11 17:49:20 +0530

    Added notification dashboard for managing notification

---
+++ app/controllers/notification_settings_controller.rb
@@ -0,0 +1,35 @@
+class NotificationSettingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def manage_notification_settings
+    if current_user.notification_setting.present?
+      @setting = current_user.notification_setting
+    else
+      @setting = current_user.build_notification_setting
+    end
+  end
+
+  def create
+    @setting = current_user.build_notification_setting(setting_params)
+    if @setting.save
+      redirect_to settings_path
+    else
+      render :manage_notification_settings, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @setting = current_user.notification_setting
+    if @setting.update(setting_params)
+      redirect_to settings_path
+    else
+      render :manage_notification_settings, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+  def setting_params
+    params.permit(:on_new_job_post, :on_removal_of_favourite_job, :on_removal_of_existing_job, :on_status_changed_on_applied, :on_job_status_changed)
+  end
+end
